2457cf2056284c6d7982c04c993c17bc8a3ce1da
Roman-Shvets <rshvets72@gmail.com>
2023-02-12 10:48:22 +0200

    Додано перевірку на вміст localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,11 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+if (localStorage.getItem(STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
 
 player.on('play', function() {
     console.log('played the video!');
@@ -13,12 +16,10 @@ player.on('play', function() {
 player.on('timeupdate', throttle(timeSaving, 1000));
 
 function timeSaving (event) {
-    //console.log('timeupdate:', event.seconds);
-    localStorage.setItem("videoplayer-current-time", event.seconds);
+    localStorage.setItem(STORAGE_KEY, event.seconds);
 }
 
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
-
